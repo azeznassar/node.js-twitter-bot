000e765d1be8c2cffae315b8affcf03822097390
Azez Nassar <azez.nassar@outlook.com>
2018-07-27 21:19:41 +1200

    .

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key:         'whFnxEvdmT9eN9ZXCKj3rmC6A',
-    consumer_secret:      'TTMmQL75WtOGKBQPiYuuF4ATKf4KGbWxxhaFPnAiXQKMZuJu2P',
-    access_token:         '1017291925332692992-5iuJLYPFvrGX0OFHCIabnP0NCo89eQ',
-    access_token_secret:  'i3F0geExjqXS2VkcHthIsYFIZJt9RDJciw0MlQN9S2AbW',
+    consumer_key:         '..',
+    consumer_secret:      '..',
+    access_token:         '..',
+    access_token_secret:  '..',
 }
